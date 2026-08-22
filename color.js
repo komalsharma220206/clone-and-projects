4d2717dc52a83da69cadffd8aee64f880092ad62
komalsharma220206 <komalsharma220206@gmail.com>
2026-08-22 18:44:01 +0530

    random

---
+++ color.js
@@ -0,0 +1,10 @@
+let body=document.body;
+function randomcolor(){
+    let val1=Math.floor(0+Math.random()*255)
+    let val2=Math.floor(0+Math.random()*255)
+    let val3=Math.floor(0+Math.random()*255)
+    return `rgb(${val1},${val2},${val3})`
+}
+setInterval(() => {
+    body.style.backgroundColor=randomcolor()
+}, 1000);
